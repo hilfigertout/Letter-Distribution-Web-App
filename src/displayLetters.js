afb6365c01f12b1b5053af3007e70d2fc213c489
hilfigertout <ijr2399@gmail.com>
2022-08-20 13:10:48 -0400

    Removed unused code

--- src/displayLetters.js
+++ src/displayLetters.js
@@ -1,24 +1,8 @@
-import { useState } from "react";
 import clsx from 'clsx';
 import { DataGrid } from '@mui/x-data-grid';
 import { Box } from "@mui/system";
 
 const DisplayLetters = ({inputString, letterCount, expectedCount}) => {   
-    const rowClass = (observed, expected) => {
-        const relativeError = (observed - expected) / expected;
-        if (observed === 0) {
-            return("zero")
-        }
-        else if (relativeError > 0.05) {
-            return ("greater")
-        }
-        else if (relativeError < -0.05) {
-            return ("less")
-        }
-        else {
-            return ("equal")
-        }
-    } 
 
     const parseRows = (letterCount, expectedCount) => {
         const rows = [];
@@ -87,39 +71,6 @@ const DisplayLetters = ({inputString, letterCount, expectedCount}) => {
         
         </div> 
         );
-
-
-    // return (
-    // <div className="display-letters">
-    //     <h4> <span className="greater">Green:</span> more than 105% of expected</h4>
-    //     <h4> <span className="less">Red:</span> less than 95% of expected</h4>
-    //     <h4> <span className="equal">Light Gray:</span> close to expected</h4>
-    //     <h4> <span className="zero">Dark Gray:</span> no observed instances of this letter</h4>
-    //     <table>
-    //         <thead>
-    //             <tr>
-    //                 <th>Letter</th>
-    //                 <th>Count</th>
-    //                 <th>Expected Count</th>
-    //             </tr>
-    //         </thead>
-    //         <tbody>
-    //             {
-    //             Object.keys(letterCount).map((letter) => (
-    //                 <tr className={rowClass(letterCount[letter], expectedCount[letter])} key={letter}>
-    //                     <td>{letter}</td>
-    //                     <td>{letterCount[letter]}</td>
-    //                     <td>{expectedCount[letter].toFixed(2)}</td>
-    //                 </tr>
-    //             ))}
-    //         </tbody>
-    //     </table>
-    //     <p>Expected letters calculated from <a href="http://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html">Cornell University Frequency Table</a>.</p>
-    //     <h3>String Entered</h3>
-    //     <p>{inputString}</p>
-    
-    // </div> 
-    // );
 }
  
 export default DisplayLetters;
