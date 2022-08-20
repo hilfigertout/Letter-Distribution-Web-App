3f47540c7b199a07b373ac107ea7787096b74251
hilfigertout <ijr2399@gmail.com>
2022-08-20 12:56:41 -0400

    Working MUI DataGrid instead of table

--- src/displayLetters.js
+++ src/displayLetters.js
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { DataGrid } from '@mui/x-data-grid';
+import { Box } from "@mui/system";
+
 const DisplayLetters = ({inputString, letterCount, expectedCount}) => {   
     const rowClass = (observed, expected) => {
         const relativeError = (observed - expected) / expected;
@@ -15,37 +19,83 @@ const DisplayLetters = ({inputString, letterCount, expectedCount}) => {
         }
     } 
 
+    const parseRows = (letterCount, expectedCount) => {
+        const rows = [];
+        let i = 0;
+        for (const letter in letterCount) {
+            let observed = letterCount[letter]
+            let expected = expectedCount[letter]
+            let proportionDiff = (observed - expected)/expected
+            rows.push({id: i, letter: letter, count: observed, expectedCount: expected, percentDifference: (100*proportionDiff)})
+            i++;
+        }
+        return rows;
+    };
+
+
+    const columns = [
+        { field: 'letter', headerName: 'Letter', width: 70},
+        { field: 'count', headerName: 'Observed Count', type: 'number', flex: 1},
+        { field: 'expectedCount', headerName: 'Expected Count', type: 'number', flex: 1},
+        { field: 'percentDifference', headerName: 'Percent Difference (%)', type: 'number', flex: 1}
+    ];
+
+    const rows = parseRows(letterCount, expectedCount);
+
+    console.log(rows);
+    console.log(columns);
+
     return (
-    <div className="display-letters">
-        <h4> <span className="greater">Green:</span> more than 105% of expected</h4>
-        <h4> <span className="less">Red:</span> less than 95% of expected</h4>
-        <h4> <span className="equal">Light Gray:</span> close to expected</h4>
-        <h4> <span className="zero">Dark Gray:</span> no observed instances of this letter</h4>
-        <table>
-            <thead>
-                <tr>
-                    <th>Letter</th>
-                    <th>Count</th>
-                    <th>Expected Count</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                Object.keys(letterCount).map((letter) => (
-                    <tr className={rowClass(letterCount[letter], expectedCount[letter])} key={letter}>
-                        <td>{letter}</td>
-                        <td>{letterCount[letter]}</td>
-                        <td>{expectedCount[letter].toFixed(2)}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-        <p>Expected letters calculated from <a href="http://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html">Cornell University Frequency Table</a>.</p>
-        <h3>String Entered</h3>
-        <p>{inputString}</p>
+        <div className="display-letters">
+            <Box sx={{height: 600, width: "60%", margin: "auto"}}>
+                <DataGrid 
+                rows={rows}
+                columns={columns}
+                pageSize={26}
+                rowsPerPageOptions={[26]}
+                disableSelectionOnClick
+                hideFooter
+                />
+                <p>Expected letters calculated from <a href="http://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html">Cornell University Frequency Table</a>.</p>
+                <h3>String Entered</h3>
+                <p>{inputString}</p>
+            </Box>
+        
+        </div> 
+        );
+
+
+    // return (
+    // <div className="display-letters">
+    //     <h4> <span className="greater">Green:</span> more than 105% of expected</h4>
+    //     <h4> <span className="less">Red:</span> less than 95% of expected</h4>
+    //     <h4> <span className="equal">Light Gray:</span> close to expected</h4>
+    //     <h4> <span className="zero">Dark Gray:</span> no observed instances of this letter</h4>
+    //     <table>
+    //         <thead>
+    //             <tr>
+    //                 <th>Letter</th>
+    //                 <th>Count</th>
+    //                 <th>Expected Count</th>
+    //             </tr>
+    //         </thead>
+    //         <tbody>
+    //             {
+    //             Object.keys(letterCount).map((letter) => (
+    //                 <tr className={rowClass(letterCount[letter], expectedCount[letter])} key={letter}>
+    //                     <td>{letter}</td>
+    //                     <td>{letterCount[letter]}</td>
+    //                     <td>{expectedCount[letter].toFixed(2)}</td>
+    //                 </tr>
+    //             ))}
+    //         </tbody>
+    //     </table>
+    //     <p>Expected letters calculated from <a href="http://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html">Cornell University Frequency Table</a>.</p>
+    //     <h3>String Entered</h3>
+    //     <p>{inputString}</p>
     
-    </div> 
-    );
+    // </div> 
+    // );
 }
  
 export default DisplayLetters;
